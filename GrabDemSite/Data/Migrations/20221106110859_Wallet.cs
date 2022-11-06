816214226bac27c38f58f072be4a14ae30341a88
Koleto <twindestroyersbgbg@gmail.com>
2022-11-06 13:10:05 +0200

    Migration

---
+++ GrabDemSite/Data/Migrations/20221106110859_Wallet.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GrabDemSite.Data.Migrations
+{
+    public partial class Wallet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Balance",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "MoneySpent",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WalletAddress",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "MoneySpent",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "WalletAddress",
+                table: "AspNetUsers");
+        }
+    }
+}
