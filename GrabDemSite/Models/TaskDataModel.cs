2a9b135969348f456cbdaa4fd0084d9bf67a4c0a
Kiril Georgiev <kiril.georgiev2003@gmail.com>
2022-11-07 20:10:46 +0200

    test commit

---
+++ GrabDemSite/Models/TaskDataModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace GrabDemSite.Models
+{
+    public class TaskDataModel
+    {
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string? Title { get; set; }
+    }
+}
---
+++ GrabDemSite/Views/Home/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using GrabDemSite.Models;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace GrabDemSite.Views.Home
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SignInManager<UserDataModel> _signInManager;
+        private readonly UserManager<UserDataModel> _userManager;
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IEmailSender _emailSender;
+        public IndexModel(
+            UserManager<UserDataModel> userManager,
+            SignInManager<UserDataModel> signInManager,
+            ILogger<IndexModel> logger,
+            IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _emailSender = emailSender;
+        }
+
+        public string? Title { get; set; } = "Test";
+    }
+}
