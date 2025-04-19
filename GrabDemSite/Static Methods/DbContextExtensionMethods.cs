1a5d1b8201ab6bc52b3477ece5deaa989b7a96ae
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-19 15:48:25 +0300

    refactor: Removed exception-handling from IO extension methods
    Removed error-handling from the extension methods as they were useless. They were just rethrowing the exception.

--- GrabDemSite/Static Methods/DbContextExtensionMethods.cs
+++ GrabDemSite/Static Methods/DbContextExtensionMethods.cs
@@ -14,153 +14,95 @@ namespace GrabDemSite.Extension_methods
         // Need summaries
         public static async Task<UserDataModel> GetUserByIdAsync(this ApplicationDbContext context, string id)
         {
-            try
-            {
-                return await context.Users.SingleAsync(x => x.Id == id);
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }   
+
+            return await context.Users.SingleAsync(x => x.Id == id);
+
+        }
         public static async Task<TaskDataModel> GetTaskAsync(this ApplicationDbContext context, UserDataModel user)
         {
-            try
-            {
-                return await context.TaskDatas.SingleAsync(x => x.User==user);
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+
+            return await context.TaskDatas.SingleAsync(x => x.User == user);
+
         }
         public static async Task<List<T>> GetDataByUserAsync<T>(this ApplicationDbContext context, UserDataModel user) where T : class
         {
-            try
+
+            if (typeof(T) == typeof(DepositDataModel))
             {
-                if (typeof(T) == typeof(DepositDataModel))
-                {
-                    return await context.DepositDatas.Where(x => x.User == user).ToListAsync() as List<T> ?? new List<T>();
-                }
-                else if (typeof(T) == typeof(WithdrawDataModel))
-                {
-                    return await context.WithdrawDatas.Where(x => x.User == user).ToListAsync() as List<T> ?? new List<T>();
-                }
-                else
-                {
-                    throw new Exception("Invalid data type.");
-                }
+                return await context.DepositDatas.Where(x => x.User == user).ToListAsync() as List<T> ?? new List<T>();
             }
-            catch (Exception)
+            else if (typeof(T) == typeof(WithdrawDataModel))
             {
-                throw;
+                return await context.WithdrawDatas.Where(x => x.User == user).ToListAsync() as List<T> ?? new List<T>();
             }
+            else
+            {
+                throw new Exception("Invalid data type.");
+            }
+
         }
         public static async Task<List<DepositDataModel>> GetDepositsByIsConfirmedAsync(this ApplicationDbContext context, UserDataModel user, bool isConfirmed = true)
         {
-            try
-            {
-                return await context.DepositDatas.Where(x => x.User.Id == user.Id && x.IsConfirmed == isConfirmed).ToListAsync();
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+
+            return await context.DepositDatas.Where(x => x.User.Id == user.Id && x.IsConfirmed == isConfirmed).ToListAsync();
+
         }
         public static async Task<UserDataModel> GetUserByNameAsync(this ApplicationDbContext context, string name)
         {
-            try
-            {
-                return await context.Users.SingleAsync(x => x.UserName == name);
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+
+            return await context.Users.SingleAsync(x => x.UserName == name);
+
         }
-        public static async Task<List<WithdrawDataModel>> GetWithdrawsByIsConfirmedAsync(this ApplicationDbContext context,string wallet, bool isConfirmed = false)
+        public static async Task<List<WithdrawDataModel>> GetWithdrawsByIsConfirmedAsync(this ApplicationDbContext context, string wallet, bool isConfirmed = false)
         {
-            try
-            {
-                return await context.WithdrawDatas.Where(x => x.WalletAddress == wallet && x.IsConfirmed == isConfirmed).ToListAsync();
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            return await context.WithdrawDatas.Where(x => x.WalletAddress == wallet && x.IsConfirmed == isConfirmed).ToListAsync();
+
         }
         public static async Task<List<WithdrawDataModel>> GetWithdrawsByIsConfirmedAsync(this ApplicationDbContext context, UserDataModel user, bool isConfirmed = false)
         {
-            try
-            {
-                return await context.WithdrawDatas.Where(x => x.User.Id == user.Id && x.IsConfirmed == isConfirmed).ToListAsync();
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            return await context.WithdrawDatas.Where(x => x.User.Id == user.Id && x.IsConfirmed == isConfirmed).ToListAsync();
+
         }
         public static async Task<UserDataModel> GetUserByInviteLinkAsync(this ApplicationDbContext context, UserDataModel user)
         {
-            try
-            {
-                return await context.Users.SingleAsync(x => x.InviteLink == user.InviteWithLink);
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+
+            return await context.Users.SingleAsync(x => x.InviteLink == user.InviteWithLink);
+
+
         }
         public static async Task AddUsersToTeamByLevelAsync(this ApplicationDbContext context, List<UserDataModel> users1, List<UserDataModel> users2)
         {
-            try
+            foreach (var user1 in users1)
             {
-                foreach (var user1 in users1)
-                {
-                    users2.AddRange(await context.Users.Where(x => x.InviteWithLink == user1.InviteLink).ToListAsync());
-                }
-                users2.OrderBy(x => x.MoneySpent);
+                users2.AddRange(await context.Users.Where(x => x.InviteWithLink == user1.InviteLink).ToListAsync());
             }
-            catch (Exception)
-            {
-                throw;
-            }
+            users2.OrderBy(x => x.MoneySpent);
+
         }
         public static async Task<List<UserDataModel>> AddUsersToTeamByLevelAsync(this ApplicationDbContext context, UserDataModel user)
         {
-            try
-            {
-                var users = await context.Users.Where(x => x.InviteWithLink == user.InviteLink).ToListAsync();
-                users.OrderBy(x => x.MoneySpent);
-                return users;
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            var users = await context.Users.Where(x => x.InviteWithLink == user.InviteLink).ToListAsync();
+            users.OrderBy(x => x.MoneySpent);
+            return users;
+
         }
         public static async Task<List<T>> LoadDataModels<T>(this ApplicationDbContext context) where T : class
         {
-            try
+
+            if (typeof(T) == typeof(DepositDataModel))
             {
-                if (typeof(T) == typeof(DepositDataModel))
-                {
-                    return await context.DepositDatas.ToListAsync() as List<T> ?? new List<T>();
-                }
-                else if (typeof(T) == typeof(WithdrawDataModel))
-                {
-                    return await context.WithdrawDatas.ToListAsync() as List<T> ?? new List<T>();
-                }
-                else if (typeof(T) == typeof(UserDataModel))
-                {
-                    return await context.Users.ToListAsync() as List<T> ?? new List<T>();
-                }
-                throw new InvalidOperationException("Unsupported type requested");
+                return await context.DepositDatas.ToListAsync() as List<T> ?? new List<T>();
             }
-            catch (Exception)
+            else if (typeof(T) == typeof(WithdrawDataModel))
             {
-                throw;
+                return await context.WithdrawDatas.ToListAsync() as List<T> ?? new List<T>();
             }
+            else if (typeof(T) == typeof(UserDataModel))
+            {
+                return await context.Users.ToListAsync() as List<T> ?? new List<T>();
+            }
+            throw new InvalidOperationException("Unsupported type requested");
+
         }
     }
 }
