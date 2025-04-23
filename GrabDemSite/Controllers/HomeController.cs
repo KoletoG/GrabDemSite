66d874605439e94ed97647ed99f461afb57fece8
KoletoG <twindestroyersbgbg@gmail.com>
2025-04-23 10:44:54 +0300

    refactor: Made Edit() a post method
    Made Edit() a post method and added AntiForgeryToken validation

--- GrabDemSite/Controllers/HomeController.cs
+++ GrabDemSite/Controllers/HomeController.cs
@@ -340,7 +340,8 @@ namespace GrabDemSite.Controllers
         /// <param name="balance">balance of the user</param>
         /// <returns>Updated user's deposits and balance</returns>
         [Authorize]
-        [HttpGet]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(string id, decimal balance)
         {
             try
